de3ec69e5e6c45fae9ff49a49b5a315422375785
amitku13 <amitkumarbhontele@gmail.com>
2024-12-30 14:12:35 +0530

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,11 +1,12 @@
+# Outputs
 output "s3_bucket_name" {
-  value = aws_s3_bucket.my_bucket[count.index].id
+  value = aws_s3_bucket.my_bucket.id
 }
 
 output "ec2_instance_public_ip" {
-  value = aws_instance.my_instance[count.index].public_ip
+  value = aws_instance.my_instance.public_ip
 }
 
 output "cloudfront_domain_name" {
-  value = aws_cloudfront_distribution.my_distribution[count.index].domain_name
+  value = aws_cloudfront_distribution.my_distribution.domain_name
 }
